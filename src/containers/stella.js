56414a129173ffb912bbcfde1bbe150d620a8861
Clement Hoang <clement.hoang24@gmail.com>
2016-01-10 00:35:19 -0500

    :sunflower: :bug: [Android] Add custom back button handler for Android

--- src/containers/stella.js
+++ src/containers/stella.js
@@ -1,36 +1,53 @@
 'use strict';
 
-import React, { Navigator, Component, View, Text } from 'react-native';
+import React, { BackAndroid, Navigator, Component, View, Text } from 'react-native';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux/native';
 import Login from '../components/login';
 import Home from '../components/home';
+import Polls from '../components/polls';
 
 const routes = {
   login: Login,
-  home: Home
+  home: Home,
+  polls: Polls
 };
 
 class Stella extends Component {
   constructor(props) {
     super(props);
+
+    BackAndroid.addEventListener('hardwareBackPress', () => {
+      if (this.refs.navigatorRef.navigationContext.currentRoute.name !== 'home') {
+        this.refs.navigatorRef.pop();
+        return true;
+      }
+
+      return false;
+    });
   }
 
   renderScene(route, navigator) {
     let Component = routes[route.name];
-    return <Component navigator={navigator} route={route} />;
+    return <Component
+      navigator={navigator}
+      route={route}
+      />;
   }
 
   render() {
     const { state, dispatch } = this.props;
 
-    return (
+    let navigatorInstance = (
       <Navigator
+        ref="navigatorRef"
         initialRoute={{ name: 'login' }}
         renderScene={this.renderScene}
         configureScene={() => { return Navigator.SceneConfigs.FloatFromRight; }}
         />
     );
+
+    return navigatorInstance;
   }
 }
 
