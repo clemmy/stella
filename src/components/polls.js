689a27e235c4db3e76f2835937ffd0c53d1ba63c
Clement Hoang <clement.hoang24@gmail.com>
2016-01-10 00:34:03 -0500

    :sunflower: [Polls] Add polls list view

---
+++ src/components/polls.js
@@ -0,0 +1,32 @@
+'use strict';
+
+import React, { StyleSheet, TouchableHighlight, Component, View, Text } from 'react-native';
+
+let styles = StyleSheet.create({
+});
+
+class Polls extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  goToPoll(event) {
+    this.props.navigator.push({
+      name: 'pollDetail'
+    });
+  }
+
+  render() {
+    const { state, dispatch } = this.props;
+
+    return (
+      <View style={styles.container}>
+        <Text>
+          POLLS VIEW ZOMGGG
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default Polls;
