707cefb1eb2c5b640c59873886aed825388fee0d
Clement Hoang <clement.hoang24@gmail.com>
2016-01-16 18:21:49 -0500

    :monkey: [logger] Make redux-logger output more readable for Immutable objects

--- src/store.js
+++ src/store.js
@@ -5,8 +5,23 @@ import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
 import reducers from './reducers';
 import INITIAL_STATE from './constants/store';
+import Immutable from 'immutable';
 
-const logger = createLogger();
+const logger = createLogger({
+  stateTransformer: (state) => {
+    let newState = {};
+
+    for (let i of Object.keys(state)) {
+      if (Immutable.Iterable.isIterable(state[i])) {
+        newState[i] = state[i].toJS();
+      } else {
+        newState[i] = state[i];
+      }
+    };
+
+    return newState;
+  }
+});
 const createStoreWithMiddleware = applyMiddleware(thunk, logger)(createStore);
 
 let store = createStoreWithMiddleware(combineReducers(reducers), INITIAL_STATE);
